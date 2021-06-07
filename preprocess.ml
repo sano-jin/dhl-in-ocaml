5238a508d95a281ff4a6887f30456f10e04a2ceb
sano-jin <sano-jin@akane.waseda.jp>
2021-06-07 14:40:22 +0900

    updated

--- preprocess.ml
+++ preprocess.ml
@@ -5,7 +5,9 @@ open Syntax
 let (<.) f g = fun x -> f (g x)
 let flip f x y = f y x  
 let uncurry f x y = f (x, y)
-		   
+let second f (a, b) = (a, f b)
+let first f (a, b) = (f a, b)
+			
 type arg' =
   | FreeLink of string
   | LocalLink of int
@@ -15,7 +17,6 @@ type atom =
   | LocalInd of int * arg'
   | FreeInd of string * arg'
 			  
-let second f (a, b) = (a, f b)
 						      
 let rec prep_arg env = function
   | Atom (p, xs) -> Atom' (p, List.map (prep_arg env) xs)
@@ -41,7 +42,7 @@ let rec prep env link_id = function
   | Rule (l, r) -> (link_id, [Either.Right (l, r)])
 
 (* returns the free/local head link names *)	     
-let check_univalent_atom (locals, frees) = function
+let collect_in_link_atom (locals, frees) = function
   | LocalInd (x, _) ->
      if List.mem x locals then failwith @@ "local link appeared more than one"
      else (x::locals, frees)
@@ -49,10 +50,10 @@ let check_univalent_atom (locals, frees) = function
      if List.mem x frees then failwith @@ "free link " ^ x ^ " appeared more than one"
      else (locals, x::frees)
 
-let check_univalent = List.fold_left check_univalent_atom ([], [])
+let collect_in_link = List.fold_left collect_in_link_atom ([], [])
 	    
 let rec update fallback f x = function
-  | [] -> fallback
+  | [] -> fallback x
   | (y, v) as h::t ->
      if x = y then (y, f v)::t
      else h::update fallback f x t
@@ -60,9 +61,13 @@ let rec update fallback f x = function
 (* collect indeg and also check the serial condition*)
 let rec collect_indeg_arg ((locals, frees) as links) = function
   | LocalLink x ->
-     (update (failwith @@ "not serial") succ x locals, frees)
+     (update (fun _ ->
+	  failwith
+	  @@ string_of_int x ^ " not serial in" ^
+	       "[" ^ String.concat "; " (List.map (fun (x, y) -> "(" ^ string_of_int x ^ ", " ^ string_of_int y ^ ")") locals) ^ "]")
+	     succ x locals, frees)
   | FreeLink x -> 
-     (locals, update ([x, 1]) succ x frees)
+     (locals, update (fun _ -> [x, 1]) succ x frees)
   | Atom' (p, xs) ->
      List.fold_left collect_indeg_arg links xs
 
@@ -72,43 +77,10 @@ let collect_indeg_atom links = function
 
 let collect_indeg = List.fold_left collect_indeg_atom 
 
-
-
-				   
-(*
-		    
-let check_serial locals atoms =
-  List.for_all (check_serial_arg locals) atoms
- *)		     
-
-	       
-
-
-				   (*
-type link_info = {
-  link_id: int option; (* Some local_link_id | None *)
-  indeg: int;
-  has_incidence: bool; (* has_head *)
-}
-type free_link_env = (string * link_info) list  (* (link_name, link_info) list *)
-type local_link_env = (int * int) list          (* (link_id, indeg) *)
-				    *)				     
-
-
-
-				   (*
-type envs = {
-   free_links: free_link_env;
-   local_links: local_link_env;
-   link_id: int; (* the seed/number of the local links that has distributed an id *)
-}
-    			  
-type rule_set = (proc' * proc') list  (* (lhs, rhs) list *)
- and proc' = {
-   atoms: atom list;
-   rules: rule_set;
-   envs: envs;
- }
-
-				    *)
+let collect_link_info proc =
+  let atoms, rules = List.partition_map (fun x -> x) @@ snd @@ prep [] 0 proc in 
+  let map_pair_zero l = List.map (fun x -> (x, 0)) l in (* monomorphism restriction *)
+  let links = first map_pair_zero @@ second map_pair_zero @@ collect_in_link atoms in
+  let link_info = collect_indeg links atoms in
+  (atoms, link_info, rules)
 
