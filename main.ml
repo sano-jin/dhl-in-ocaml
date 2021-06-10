7727e814b1442aab477ca0ecd562c169b6f49f0d
sano-jin <sano-jin@akane.waseda.jp>
2021-06-11 00:40:22 +0900

    updated

--- main.ml
+++ main.ml
@@ -30,8 +30,8 @@ let test_syntax =
 let prep =
   Preprocess.prep <. parse <. read_file
 
-let find_atoms file_name atom_list =
-  let Preprocess.Rule' (((atoms, (indegs, _)), _), _) = List.hd @@ snd @@ prep file_name in
+let test file_name i atom_list =
+  let Preprocess.Rule' (((atoms, (indegs, _)), _), _) = flip List.nth i @@ snd @@ prep file_name in
   Findatom.find_atoms indegs atom_list atoms 
 
 let test_atom_list =
@@ -49,11 +49,22 @@ let test_atom_list =
 			    ])
 	       ])
 
+let test_atom_list_nil =
+  Vm.test_atom2atom_list @@
+    Vm.TAtom ("test", [
+		 Vm.TAtom ("append", [
+			      Vm.TAtom ("nil", []);
+			      Vm.TAtom ("cons", [
+					   Vm.TAtom ("cons", []);
+					   Vm.TAtom ("nil", [])
+					 ])
+			    ])
+	       ])
+
+	     
 let dump =
   Vm.dump_atoms test_atom_list
     
-let test =
-  flip find_atoms test_atom_list
   
 (*
 
