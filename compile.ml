094af2762271bf6ff00642e5ccb2af79ef62b2f7
sano-jin <sano-jin@akane.waseda.jp>
2021-06-10 22:54:16 +0900

    updated

--- compile.ml
+++ compile.ml
@@ -2,14 +2,15 @@
 
 open Preprocess
 
-(* instruction *)
-type inst =
-  | FindAtom of string * int * int   (* FindAtom (atom_name, arity) *)
-  | Neq of int * int                 (* Neq (atom_reg_i_1, atom_reg_i_2) *)
-  | Eq of int * int                  (* Eq (atom_reg_i_1, atom_reg_i_2) *)
-  | Deref of int * int               (* Deref (atom_reg_i, link_i) *)
-  | IndegLe of int * int             (* IndegLe (atom_reg_i, indeg) *)
-  | IndegEq of int * int             (* IndegEq (atom_reg_i, indeg) *)
-  | Func of int * string * int       (* Func (atom_reg_i, atom_name, arity) *)
-        
-      
+type c_arg =
+  | CFreeLink of string
+  | CLocalLink of int
+
+type c_atom = string * c_arg list
+
+type c_ind =
+  | CLocalInd of int * c_atom
+  | CFreeInd of string * c_atom
+  | CRedir of string * string
+
+			 
