d9e752ce08f38d5bfdcb71ebd61afca0badb2989
sano-jin <sano-jin@akane.waseda.jp>
2021-06-12 00:26:56 +0900

    udpated

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-make
-./dhl
-open Main;;
-test "test/append.dhl" 0 test_atom_list;;
-test "test/append.dhl" 1 test_atom_list_nil;;
-test "test/append.dhl" 1 test_atom_list;;
-test "test/append.dhl" 0 test_atom_list_nil;;
-exit 0;;
