6485cb07a2cd4f308bce9379d5bfbae08e6cc621
sano-jin <sano-jin@akane.waseda.jp>
2021-06-25 23:45:58 +0900

    updated

---
+++ test/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+test () {
+    echo "testing $1"
+    opam exec -- dune exec dhl -- ../example/$1.dhl -t > tmp/output_$1.log
+    colordiff tmp/output_$1.log expected/expected_output_$1.log || exit
+}
+echo testing
+mkdir tmp
+test append
+test test
+test test2
+test test3
+test test4
+test test5
+
+rm -rf tmp
